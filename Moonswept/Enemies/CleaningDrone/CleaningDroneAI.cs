63b8388dd2381a1615c4d07f6e30ecf9c04db385
Test-Account666 <36412486+Test-Account666@users.noreply.github.com>
2025-11-15 09:23:47 +0100

    Improve CleaningDrone performance by adding a Fog Pool

--- Moonswept/Enemies/CleaningDrone/CleaningDroneAI.cs
+++ Moonswept/Enemies/CleaningDrone/CleaningDroneAI.cs
@@ -1,6 +1,6 @@
 using GameNetcodeStuff;
-using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Moonswept.Enemies.CleaningDrone;
 
@@ -20,24 +20,86 @@ public class CleaningDroneAI : EnemyAI {
     private const float _DEFAULT_SPEED = 2F;
     private const float _RETREAT_SPEED = 14F;
 
+    private const int _FOG_POOL_INITIAL_CAPACITY = 8;
+    private const int _FOG_POOL_MAX_SIZE = 64;
+
     public enum BehaviourState {
         WANDER,
         RETREAT,
     }
 
+    /*
+    Shared pool for all CleaningDroneAI instances (single pool for the fog prefab type).
+    This is initialized once in Start() of the first instance that has a non-null fogPrefab.
+    */
+    public static ObjectPool<GameObject>? FogPool;
+
     public override void Start() {
         base.Start();
-        currentBehaviourStateIndex = (int) BehaviourState.WANDER;
+
+        currentBehaviourStateIndex = (int)BehaviourState.WANDER;
         StartSearch(transform.position);
+
+        InitializeFogPool();
+    }
+
+    private void InitializeFogPool() {
+        if (fogPrefab == null) return;
+
+        FogPool ??= new(
+            createFunc: () => {
+                var fog = Instantiate(fogPrefab);
+                fog.SetActive(false);
+                return fog;
+            },
+            actionOnGet: fog => {
+                fog.SetActive(true);
+                var fogBehavior = fog.GetComponent<FogBehavior>();
+                fogBehavior.ResetForReuse();
+            },
+            actionOnRelease: fog => {
+                var fogBehavior = fog.GetComponent<FogBehavior>();
+                fogBehavior.OnRelease();
+                fog.SetActive(false);
+            },
+            actionOnDestroy: Destroy,
+            collectionCheck: false,
+            defaultCapacity: _FOG_POOL_INITIAL_CAPACITY,
+            maxSize: _FOG_POOL_MAX_SIZE
+        );
     }
 
     public override void Update() {
         base.Update();
 
-        modelRoot.transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.fixedDeltaTime);
-        _movementStopwatch += Time.fixedDeltaTime;
+        if (isEnemyDead) return;
+
+        _gasStopwatch += Time.deltaTime;
+
+        if (_gasStopwatch >= _GAS_DISPENSE_INTERVAL) {
+            _gasStopwatch -= _GAS_DISPENSE_INTERVAL;
+            SpawnFog();
+        }
+
+        modelRoot.transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
+
+        _movementStopwatch += Time.deltaTime;
         if (_movementStopwatch >= 4F) _movementStopwatch = 0;
         modelRoot.transform.localPosition = new(0, 6.24F + movement.Evaluate(_movementStopwatch) * 4, 0);
+    }
+
+    private void SpawnFog() {
+        var spawnPosition = modelRoot.transform.position;
+        var spawnRotation = Quaternion.identity;
+
+        var instance = FogPool?.Get();
+        if (instance is null || !instance) {
+            Moonswept.Logger.LogWarning("Failed to get fog instance from pool! Is the pool empty?");
+            return;
+        }
+
+        instance.transform.position = spawnPosition;
+        instance.transform.rotation = spawnRotation;
     }
 
     public override void DoAIInterval() {
@@ -45,14 +107,7 @@ public class CleaningDroneAI : EnemyAI {
 
         if (isEnemyDead) return;
 
-        _gasStopwatch += AIIntervalTime;
-
-        if (_gasStopwatch >= _GAS_DISPENSE_INTERVAL) {
-            _gasStopwatch = 0F;
-            SpawnFogClientRpc();
-        }
-
-        switch ((BehaviourState) currentBehaviourStateIndex) {
+        switch ((BehaviourState)currentBehaviourStateIndex) {
             case BehaviourState.WANDER:
                 agent.speed = _DEFAULT_SPEED;
                 return;
@@ -73,7 +128,7 @@ public class CleaningDroneAI : EnemyAI {
 
         if (current / init <= 0.4F) {
             StartSearch(transform.position);
-            SwitchToBehaviourState((int) BehaviourState.WANDER);
+            SwitchToBehaviourState((int)BehaviourState.WANDER);
             return;
         }
 
@@ -84,11 +139,8 @@ public class CleaningDroneAI : EnemyAI {
         base.HitEnemy(force, playerWhoHit, playHitSfx, hitID);
 
         StopSearch(currentSearch);
-        SwitchToBehaviourState((int) BehaviourState.RETREAT);
+        SwitchToBehaviourState((int)BehaviourState.RETREAT);
         _initialPos = transform.position;
         _currentTargetNode = ChooseFarthestNodeFromPosition(transform.position);
     }
-
-    [ClientRpc]
-    public void SpawnFogClientRpc() => Instantiate(fogPrefab, modelRoot.transform.position, Quaternion.identity);
 }
--- Moonswept/Enemies/CleaningDrone/FogBehavior.cs
+++ Moonswept/Enemies/CleaningDrone/FogBehavior.cs
@@ -12,23 +12,50 @@ public class FogBehavior : MonoBehaviour {
     private float _stopwatch;
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
+    // Reset state when the pooled object is taken from the pool
+    public void ResetForReuse() {
+        _stopwatch = 0f;
+
+        particleSystem.Clear(true);
+        particleSystem.Play(true);
+
+        localFog.enabled = true;
+        collider.enabled = true;
+        gameObject.SetActive(true);
+    }
+
+    // Called when the pooled object is released back to the pool
+    public void OnRelease() {
+        if (particleSystem.isPlaying) particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
+        localFog.enabled = false;
+        collider.enabled = false;
+    }
+
     public void FixedUpdate() {
-        //TODO: Possibly improve this?
-        foreach (var playerScript in StartOfRound.Instance.allPlayerScripts) {
-            if (!playerScript || playerScript.isPlayerDead || !playerScript.isPlayerControlled) continue;
-
-            if (!collider.bounds.Contains(playerScript.playerEye.position)) continue;
-
-            playerScript.drunknessInertia = Mathf.Clamp(playerScript.drunknessInertia + Time.fixedDeltaTime / 2F * playerScript.drunknessSpeed, 0.1F, 4.5F);
-            playerScript.increasingDrunknessThisFrame = true;
-        }
+        ApplyFogEffects();
 
         _stopwatch += Time.fixedDeltaTime;
         if (_stopwatch >= destroyAfter - 1.5F) {
-            particleSystem.Stop();
-            Destroy(localFog);
+            if (particleSystem.isPlaying) particleSystem.Stop();
+            localFog.enabled = false;
         }
 
-        if (_stopwatch >= destroyAfter) Destroy(gameObject);
+        if (_stopwatch < destroyAfter) return;
+        // return to pool for reuse; pool's actionOnRelease will deactivate & OnRelease will be called
+        CleaningDroneAI.FogPool?.Release(gameObject);
+    }
+
+    private void ApplyFogEffects() {
+        foreach (var playerScript in StartOfRound.Instance.allPlayerScripts) {
+            if (!playerScript || playerScript.isPlayerDead || !playerScript.isPlayerControlled) continue;
+            if (!collider.bounds.Contains(playerScript.playerEye.position)) continue;
+            if (playerScript.drunknessInertia >= 4.5F) continue;
+
+            var drunkness = playerScript.drunknessInertia + Time.fixedDeltaTime / 2F * playerScript.drunknessSpeed;
+
+            playerScript.drunknessInertia = Mathf.Clamp(drunkness, 0.1F, 4.5F);
+            playerScript.increasingDrunknessThisFrame = true;
+        }
     }
 }
